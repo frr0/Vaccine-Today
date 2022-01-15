f389a9cf5c730884697eaa065fd2070561d79ec9
fra <fra.sette.p.c@gmail.com>
2022-01-15 18:33:30 +0100

    2* pdf

--- program.sh
+++ program.sh
@@ -20,7 +20,7 @@ wget -q https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master
 wget -q https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/vaccini-summary-latest.csv
 
 pandoc -V geometry:"paperwidth=320mm, paperheight=210mm, top=2cm, bottom=1.5cm, left=2cm, right=2cm" -o anagrafica-vaccini-summary-latest.pdf anagrafica-vaccini-summary-latest.csv
-pandoc -V geometry:"paperwidth=420mm, paperheight=210mm, top=2cm, bottom=1.5cm, left=2cm, right=2cm" -o vaccini-summary-latest.pdf anagrafica-vaccini-summary-latest.csv
+pandoc -V geometry:"paperwidth=420mm, paperheight=210mm, top=2cm, bottom=1.5cm, left=2cm, right=2cm" -o vaccini-summary-latest.pdf vaccini-summary-latest.csv
 
 xdg-open anagrafica-vaccini-summary-latest.pdf
 xdg-open vaccini-summary-latest.pdf
